a1277d9a13690f44a314dd797105ab3240cc2d69
Lebo <mathotolebogang@gmail.com>
2024-03-05 10:07:56 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LandingComponent } from './components/landing/landing.component';
+import { SharedServiceComponent } from './services/shared-service/shared-service.component';
+import { LoginComponent } from './popups/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SharedServiceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
