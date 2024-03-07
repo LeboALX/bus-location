9cd39169897d2a30f0bc6da78010bcf33e192ed7
Edward <edwardthupane80@gmail.com>
2024-03-07 10:42:52 +0200

    Latest landing

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterComponent } from 'src/app/popups/register/register.component';
 
 @Component({
   selector: 'app-landing',
@@ -8,20 +10,19 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LandingComponent {
   dialogItems: string[] = ['Driver','Admin'];
+  currentValue: string = '';
+
+  onInput(event:any){
+    this.currentValue = event.target.value;
+  }
   
   searchBus!: FormGroup;
   panelColor = new FormControl;
 
-  name:string = 'joel'
-
-  constructor() {
+  constructor(private dialog: MatDialog) {
     this.searchBus = new FormGroup({
       shiftNumber: new FormControl('', [Validators.required, Validators.maxLength(6)])
     }) 
   }
-
-  log(indx:any) {
-    console.log(indx)
-  }
-
+  
 }
