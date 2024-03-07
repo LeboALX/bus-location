506b3240ae8d17b1593ab00fd22a5d50d864e62b
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-03-07 15:23:12 +0200

    tracker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,26 @@ import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './popups/login/login.component';
-import { SuperAdminComponent } from './components/super-admin/super-admin.component'
+import { SuperAdminComponent } from './components/super-admin/super-admin.component';
+import { TrackerComponent } from './components/tracker/tracker.component'
+import { GoogleMapsModule } from '@angular/google-maps';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     LoginComponent,
-    SuperAdminComponent
+    SuperAdminComponent,
+    TrackerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
