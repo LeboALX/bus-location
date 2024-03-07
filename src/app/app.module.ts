414af7500d6e8d951ee575a937ce8052fbe0bc4f
Lebo <mathotolebogang@gmail.com>
2024-03-07 10:03:39 +0200

    temp commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,30 @@ import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './popups/login/login.component';
-import { SuperAdminComponent } from './components/super-admin/super-admin.component'
+import { SuperAdminComponent } from './components/super-admin/super-admin.component';
+import { NgChartsModule } from 'ng2-charts';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { AddRouteComponent } from './popups/add-route/add-route.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     LoginComponent,
-    SuperAdminComponent
+    SuperAdminComponent,
+    PieChartComponent,
+    LineChartComponent,
+    AddRouteComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
