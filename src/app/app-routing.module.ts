ab411ad2cda0ea06fcc3f2eeeee4f3ff8ce283f8
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-03-07 15:31:52 +0200

    tracker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { AddVehicleComponent } from './components/add-vehicle/add-vehicle.component';
-import { AddDriverComponent } from './components/add-driver/add-driver.component';
-import { AddTripComponent } from './components/add-trip/add-trip.component';
-import { CompanyDashboardComponent } from './components/company-dashboard/company-dashboard.component';
-=======
-import { LoginComponent } from './popups/login/login.component';
-import { AddBusesComponent } from './popups/add-buses/add-buses.component';
->>>>>>> 83d5d3221eec246c0ecdf728fb855aa69c62e3fe
 
-const routes: Routes = [
-  { path: '', redirectTo: '/company-dashboard', pathMatch: 'full' }, // redirect to `sign-in`
-  { path: 'company-dashboard', component: CompanyDashboardComponent ,children: [
-    {path: 'add-vehicle', component: AddVehicleComponent},
-    {path: 'add-driver', component: AddDriverComponent},
-    {path: 'add-trip', component: AddTripComponent}
-  ]}
-  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
