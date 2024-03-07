e88fc7ec8f31eb369a3b9205fd0ab66ab4a4dde2
Lebo <mathotolebogang@gmail.com>
2024-03-07 12:07:50 +0200

    msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './popups/login/login.component';
 import { SuperAdminComponent } from './components/super-admin/super-admin.component';
-<<<<<<< HEAD
 import { NgChartsModule } from 'ng2-charts';
 import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
 import { LineChartComponent } from './charts/line-chart/line-chart.component';
 import { AddRouteComponent } from './popups/add-route/add-route.component';
-=======
 import { AddBusesComponent } from './popups/add-buses/add-buses.component';
 import { RegisterComponent } from './popups/register/register.component';
 import { AddDriverComponent } from './popups/add-driver/add-driver.component'
->>>>>>> c14f8732762f12722f72569aa10183dac50ebb90
 
 @NgModule({
   declarations: [
@@ -26,17 +23,12 @@ import { AddDriverComponent } from './popups/add-driver/add-driver.component'
     LandingComponent,
     LoginComponent,
     SuperAdminComponent,
-<<<<<<< HEAD
     PieChartComponent,
     LineChartComponent,
     AddRouteComponent,
-   
-=======
-    AddBusesComponent
-   
+    AddBusesComponent,
     RegisterComponent,
     AddDriverComponent
->>>>>>> c14f8732762f12722f72569aa10183dac50ebb90
   ],
   imports: [
     BrowserModule,
