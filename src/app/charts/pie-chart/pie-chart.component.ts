9a531826c66a92d937735cbe0eaf8fe0352fd731
Edward <edwardthupane80@gmail.com>
2024-03-11 09:10:25 +0200

    Latest code

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartOptions } from 'chart.js';
+import { ApiService } from 'src/app/services/api.service';
+import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.scss']
 })
-export class PieChartComponent {
-  @Input()data:number = 0
+export class PieChartComponent implements OnInit {
   public pieChartOptions: ChartOptions<'pie'> = {
     responsive: false,
     plugins: {
@@ -16,14 +17,36 @@ export class PieChartComponent {
       }
     }
   };
-  public pieChartLabels = [ ['Pending'], ['Declined'], ['Approved']];
-  public pieChartDatasets = [ {
-    data: [ [this.data], 30, 50 ], 
-    backgroundColor: ['orange', '#f00101', '#3cba5f']
-  } ];
+  public pieChartLabels = [['Pending'], ['Declined'], ['Approved']];
+  public pieChartDatasets: any;
   public pieChartLegend = true;
   public pieChartPlugins = [];
 
-  constructor() {
+  constructor(private api: ApiService, private sharedService: SharedService) {
+    this.refreshPie();
+    this.sharedService.watchPieUpdates().subscribe((changes: any) => {
+      this.refreshPie();
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  refreshPie(): void {
+    this.api.genericGet('/get-company')
+      .subscribe({
+        next: (res: any) => {
+          const pendingCount = res.filter((company: any) => company.status.toLowerCase() === 'pending').length
+          const declinedCount = res.filter((company: any) => company.status.toLowerCase() === 'declined').length
+          const approvedCount = res.filter((company: any) => company.status.toLowerCase() === 'approved').length
+
+          this.pieChartDatasets = [{
+            data: [pendingCount, declinedCount, approvedCount],
+            backgroundColor: ['orange', '#f00101', '#3cba5f']
+          }];
+        },
+        error: (err: any) => console.log('Error', err),
+        complete: () => { }
+      });
   }
 }
