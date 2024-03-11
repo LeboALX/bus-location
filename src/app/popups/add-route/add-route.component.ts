4d3de8ad4238d8865cd5fbc4d9254ddc1433d1ad
KgothatsoTheko <kgothatsotheko7@gmail.com>
2024-03-11 11:28:52 +0200

    Tracker complete

--- src/app/popups/add-route/add-route.component.ts
+++ src/app/popups/add-route/add-route.component.ts
@@ -15,6 +15,14 @@ export class AddRouteComponent {
   stops: any[] = ['Bryanston', 'Brixton', 'Sandton', 'Hillbrow', 'Westgate', 'Parktown', 'Alex'];
   destination: any[] = ['Randburg', 'Fourways', 'Midrand', 'Kyalami', 'Woodmead', 'Vaal'];
 
+  stopsLocations = [{ title: 'Bryanston', position: { lat: -26.090532, lng: 28.026145 } },
+  { title: "Brixton", position: { lat: -26.10192, lng: 28.040242 } },
+  { title: "Sandton", position: { lat: -26.091321, lng: 28.023776 } },
+  { title: "Hillbrow", position: { lat: -26.14906, lng: 28.009492 } },
+  { title: "Westgate", position: { lat: -26.182785, lng: 28.017755 } },
+  { title: "Parktown", position: { lat: -26.201923, lng: 28.03123 } },
+  { title: "Alex", position: { lat: -26.1086844, lng: 28.0582833 } }]
+
   constructor(private snackbar: MatSnackBar, private api: ApiService, @Inject(MAT_DIALOG_DATA) public _data: any) {
     if (_data._data) {
       this.addRouteForm = new FormGroup({
@@ -34,10 +42,22 @@ export class AddRouteComponent {
   }
 
   submit() {
-    if(this.addRouteForm.invalid) {
+    let busChosenStopsAndLocations: any = [];
+
+    if (this.addRouteForm.invalid) {
       this.snackbar.open('All fields are required', 'Ok', { duration: 3000 });
       return;
     };
+
+    this.addRouteForm.value.stops.forEach((chosenStop: any) => {
+      this.stopsLocations.forEach((stop: any) => {
+        if (chosenStop.toLowerCase() === stop.title.toLowerCase()) {
+          busChosenStopsAndLocations.push(stop);
+        }
+      })
+    })
+    this.addRouteForm.value.stops = busChosenStopsAndLocations;
+
     this.api.genericPost('/add-trip', this.addRouteForm.value)
       .subscribe({
         next: (res: any) => {
